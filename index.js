b27c8f9871f897ec14c6943252e99b1bbe312601
Clif <clifhodges13@gmail.com>
2020-01-15 18:51:32 -0600

    created router for endpoints

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const router = require("./router")
+
+const server = express()
+server.use("/", router)
+server.use(express.json())
+
+const HOST = process.env.HOST || "0.0.0.0"
+const PORT = process.env.PORT || 8000
+
+server.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}.`))
